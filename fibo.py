b9fb762409c536b1bf6f8f92526f6b02851015b7
jorge <jorge@Jorge-PC.localdomain>
2019-02-17 20:46:43 -0600

    fibo serie

---
+++ fibo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+print("inserte el primer término de la serie Fibo")
+
+def fibo(n):
+    if n == 0:
+        return print("de a cuerdo!")
+    else:
+        return print("incio incorrecto")
+    
+i = int(input())
+
+fibo(i)
+
+def Fob(n):
+    a = 0
+    b = 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+
+print(Fob(500))
+
+
+
+
