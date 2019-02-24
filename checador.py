d6414e989f0247c057290c202e08db80243bd9c4
jorge <jorge@Jorge-PC.localdomain>
2019-02-24 16:44:17 -0600

    checador = primos

---
+++ checador.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def checador(n):
+        for i in range(2, n):
+                    if (n % i) == 0:
+                                    return print(n, "no es primo")
+            
+
+print("ingrese número")
+checador(int(input()))
